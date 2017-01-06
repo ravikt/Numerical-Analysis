241450a4519d50000f558c09812ea085b7dfdddf
ravikt <rvthkr4@gmail.com>
2017-01-06 14:44:43 +0530

    update

--- non_lin.m
+++ non_lin.m
@@ -26,16 +26,27 @@ f3 = (((1 - x^(m+1))*(1 - y^(n+1))*b0) / ((1-x)*(1-y))) - t;
 
 % Calculation of jacobian
 
-J = [diff(f1,a),diff(f1,b),diff(f1,t);diff(f2,a),diff(f2,b),diff(f2,t);...
+ J = [diff(f1,a),diff(f1,b),diff(f1,t);diff(f2,a),diff(f2,b),diff(f2,t);...
     diff(f3,a),diff(f3,b),diff(f3,t);];
-Jinv = inv(J);
-
-soln = [ai,bi,ti];
-
+% Jinv = inv(J);
+% 
+prev_sol = [ai,bi,ti];
+% 
 for i=1:10
-   jacob = subs(Jinv,{a,b,t},soln); 
-      x1 = subs(f1,{a,b,t},soln);
-      x2 = subs(f2,{a,b,t},soln);
-      x3 = subs(f3,{a,b,t},soln);
-    soln = jacob*[x1;x2;x3];  
+ 
+   jacob = subs(J,{a,b,t},prev_sol); 
+   j=inv(jacob);
+   x1 = subs(f1,{a,b,t},prev_sol);
+   x2 = subs(f2,{a,b,t},prev_sol);
+   x3 = subs(f3,{a,b,t},prev_sol);
+   next_sol = prev_sol' - j*[x1;x2;x3];  
+   prev_sol = next_sol';
+   
 end
+% for i=1:10
+%    jacob = subs(Jinv,{a,b,t},soln); 
+%       x1 = subs(f1,{a,b,t},soln);
+%       x2 = subs(f2,{a,b,t},soln);
+%       x3 = subs(f3,{a,b,t},soln);
+%     soln = jacob*[x1;x2;x3];  
+% end
